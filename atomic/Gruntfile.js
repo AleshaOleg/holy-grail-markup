73d353542d888f95c17edebc685b348f3a627b88
Oleh Aloshkin <olegaleshkin@gmail.com>
2016-08-03 18:11:25 +0300

    Setup Gruntfile for Atomic

--- atomic/Gruntfile.js
+++ atomic/Gruntfile.js
@@ -1,17 +1,27 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        watch: {
+            dev: {
+                options: {
+                    livereload: true
+                },
+                files: [
+                    './*.html'
+                ],
+                tasks: ['atomizer']
+            }
+        },
         atomizer: {
-            files: [
-                {
+            files: {
                     src: ['./*.html'],
                     dest: './style.css'
-                }
-            ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-atomizer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['atomizer']);
+    grunt.registerTask('default', ['watch']);
 };
